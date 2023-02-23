754a8121bb60a59cd5f8863d0041c67f76426849
Alex Bejan <>
2023-02-23 19:43:32 +0200

    fix

--- src/lib/components/Chat/Chat.svelte
+++ src/lib/components/Chat/Chat.svelte
@@ -116,7 +116,7 @@
 </script>
 
 <div class="root">
-	<div class="history md:p-2 overflow-y-scroll h-[350px] md:h-[450px]" bind:this={div}>
+	<div class="history md:p-2 overflow-y-scroll h-[350px] md:h-[450px] pr-2" bind:this={div}>
 		<ul>
 			{#each messages as message, i (message.time)}
 				{#if message.text}
