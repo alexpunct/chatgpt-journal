34bc5a8f628e7ddf11758879de6333d88cc6d89a
Alex Bejan <>
2023-03-06 12:23:17 +0200

    small fix for edit form to not show scrollbars

--- src/routes/(inner)/journal/[id]/EditJournalEntryForm.svelte
+++ src/routes/(inner)/journal/[id]/EditJournalEntryForm.svelte
@@ -94,7 +94,7 @@
 		<div class="p-0">
 			<div class="relative">
 				<pre
-					class="invisible !text-[10px]"
+					class="invisible !text-[11px]"
 					aria-hidden="true"
 					style="min-height: {minHeight}; max-height: {maxHeight}">{content + '\n\n'}</pre>
 				<textarea
