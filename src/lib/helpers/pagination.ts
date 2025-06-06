681f36668804c347109e5b8f36401bf8ce2a6690
Alex Bejan <alexbejan05@gmail.com>
2025-06-06 06:53:09 +0100

    fix pagination offset

--- src/lib/helpers/pagination.ts
+++ src/lib/helpers/pagination.ts
@@ -4,10 +4,15 @@ export const supabasePaginationDefaults = {
 	amounts: [1, 5, 20]
 };
 
-export const getFromTo = (page: number, size: number) => {
-	const limit = size ? +size : supabasePaginationDefaults.limit;
-	const from = page ? page * limit : supabasePaginationDefaults.offset;
-	const to = page ? from + size : size;
+/**
+ * Calculate the range for Supabase queries using an offset and limit.
+ * @param {number} offset - Zero based starting index.
+ * @param {number} limit - Amount of records to fetch.
+ */
+export const getFromTo = (offset: number, limit: number) => {
+	const _limit = isNaN(limit) ? supabasePaginationDefaults.limit : +limit;
+	const from = isNaN(offset) ? supabasePaginationDefaults.offset : offset;
+	const to = from + _limit - 1;
 
 	return { from, to };
 };
